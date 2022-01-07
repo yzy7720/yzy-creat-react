9b31bc0500f4cfec77ed8f0cebd71ff5133d765d
yzy7720 <740343685@qq.com>
2022-01-07 16:24:27 +0800

    fix: commit new react project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-07 15:34:50
+ * @LastEditTime: 2022-01-07 16:22:46
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \yzy-creat-react\src\App.tsx
+ */
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      自己测试搭建react
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
+/*
+ * @Author: yizhiyu
+ * @Date: 2022-01-07 15:34:50
+ * @LastEditTime: 2022-01-07 16:20:04
+ * @LastEditors: yizhiyu
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \yzy-creat-react\src\index.tsx
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +18,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
