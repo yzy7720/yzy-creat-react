7092528f9b355049f7f6a1f71577623fbfa08995
yzy7720 <740343685@qq.com>
2022-01-07 18:47:56 +0800

    fix: main

--- src/router/main.tsx
+++ src/router/main.tsx
@@ -3,11 +3,11 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/home';
 import Login from '../pages/login';
 
-const main = () => (
+const Main = () => (
   <Routes>
     <Route path='/home' element={Home} />
     <Route path='/login' element={Login} />
     <Route path='*' element={<Navigate to="/login" />} />
   </Routes>
 )
-export default main;
+export default Main;
